e08ea83b677a55a42837ba204d05eabad2e48a4b
Justin van Emmerik <jvanemmerik@brightcove.com>
2023-11-20 14:15:17 +1100

    New commit.

---
+++ README.md
@@ -0,0 +1,164 @@
+# Brightcove - cue point markers plugin
+
+A Brightcove player plugin that adds cue point markers and associated metadata dynamically to the player progress bar.
+The plugn prioritises the cue metadata form Video Cloud over the "chapter" metadata associated with the video object.\
+Unlike other implementations of chaptering there is no need for the admin to add an index or marker at `00:00` of the video.\
+\
+This implementation has been tested against the Brightcove player version 7.2 and 6.67.\
+Browser testing has been mild but **Firefox 110.0.1**, **Chrome 111.0.5563.64** and **Safari 16.3** have done the rounds. Edge requires a review.\
+\
+**What remains untested:** The running of this plugin alongside the IMA ads plugin needs more thorough testing in the context of Server Side Ad Insertion and Client Side Ad Insertion where chapters are used to trigger mid-roll video ads.
+
+### Behaviours
+
+![video player](readme_images/video_player.gif)
+
+* This plugin works with a [playlist enabled player](https://studio.support.brightcove.com/get-started/basics/video-cloud-basics-creating-playlist-player.html)
+* Tooltips with chapter description appear alongside the chapter marker when hovered over on the player control bar
+* Ad based cue points are ignored
+* The colour of the cue markers can be defined in the Video Cloud player module as a JSON option
+* Once chapters have been added the change will be detected by all deployed Brightcove players that have this plugin installed
+* Intended for desktop web - Cue point elements have been hidden for mobile intentionally
+
+#### Repo structure and purpose
+<table>
+  <thead>
+    <tr>
+      <th>Folder/Filename</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>index.html</td>
+      <td>Basic finctionality of the plugin. HTML, CSS and JavaScript, in a single file.</td>
+    </tr>
+    <tr>
+      <td>plugin/index.html</td>
+      <td>The browsable HTML page that contains the HTML code, and the link to the CSS and the include to the JavaScript.</td>
+    </tr>
+    <tr>
+      <td>plugin/cuepoints.js</td>
+      <td>The JavaScript for the plugin.</td>
+    </tr>
+    <tr>
+      <td>plugin/cuepoints.css</td>
+      <td>The CSS for the plugin.</td>
+    </tr>
+  </tbody>
+</table>
+
+### Usage
+
+#### Method 1: Chapters in the long description field
+
+To have the chapters appear on the time player progress bar this plugin must be installed on a designated player.
+Chapters can be pasted or typed into the [long description field](https://studio.support.brightcove.com/media/properties/editing-video-properties-using-media-module.html#videoinfo) in the media module or the [add code cue points using the cue points UI](https://studio.support.brightcove.com/media/general/working-cue-points-media-module.html).
+
+![edit long description](readme_images/long_description.gif)
+
+> **Note** The following chapters are an example of what can be placed in the long description field.
+```
+00:10 - Snow Hey Oh
+01:04 - Sikamikanico
+03:14 - Higher Ground
+05:44 - Good Time Boys
+06:51 - Scar Tissue
+11:04 - By the Way
+11:21 - LA Lakers
+11:46 - Dark Necessities
+12:20 - Aeroplane
+```
+Or
+```
+00:10 Agoraphobia
+01:04 Aqueous Transmission
+03:14 Drive
+05:44 Are You In
+06:51 Nice to Know You
+11:04 Privilege
+11:21 Wish you were here
+11:46 Pardon Me
+12:20 Megalomaniac
+```
+> Timestamps will be recognised in `HH:MM:SS`, `H:MM:SS`, `MM:SS` or `M:SS` formats.
+
+In the above example there are hyphens but these are unnecessary to make make the plugin recognise chapters. Most characters such as `+`, `*`, `~`, `:` or `-` will be stripped so the "chapter tip" or "cue tip" will display correctly. Whitespace is also trimmed from the chapter description as well.\
+Chapters or indexes that have timecode in the description will be ignored.\
+For eample:
+```
+The sunrise 20:34
+```
+Will be ignored.\
+\
+Additionally time in seconds will be ignored.
+```
+120 Chapter name
+```
+> **Note** If the plugin detects a chapter duration that is greater than the video duration the illegal chapter will also be ignored.
+
+Formats that will work:
+```
+02:32 No More Tears
+2:32 - Perry Mason
+0:23 Crazy Train
+0:46
+00:30 + Bark at the Moon
+02:05:17 Mr Crowley
+1:25:48 Patient Number 9
+```
+As mentioned the there is no need for a `00:00` chapter or "intro" as with **YouTube** to trigger the inclusion and recognition of the chapters.
+> **Note** With little effort (for the tecchnically inclined) the plugin can be augmented to accept chapter data from a [custom field](https://studio.support.brightcove.com/admin/creating-custom-metadata-fields.html) to suit a more specific workflows.
+
+> **Note** **Remember:** With this plugin you can use [cue points](https://studio.support.brightcove.com/media/general/working-cue-points-media-module.html) at the same time. Timestamps that are the same will be removed and the cue point metadata will override the chapter in the long description field.\
+<strong style="color: hotpink;">Use `'CODE'` cue points. They will render on the player in the same way</strong> as chapters found in the long description field.
+
+#### Method 2: Use cue points
+
+![add cue points](readme_images/cue_points.gif)
+
+This plugin is built to work with, without, or both chapters and cue points. Any duplication of events will be removed where the `'CODE'` based cue data is prioritised.\
+Chapter metadata that is taken from the long description field is classified as a `'TEXT'` cue point.\
+Both approaches can be used at the same time.
+
+> **Note** Ad cue points are reserved to support advertsing activity for a video object and are ignored by this plugin.
+
+More information on adding cue points and their types can be found here:
+https://studio.support.brightcove.com/media/general/working-cue-points-media-module.html
+
+#### Cue marker colours
+When the plugin is being configured a **Video Cloud** user has the option of setting the marker colour as follows:
+
+```json
+{
+    "cue_marker_color": "#FF69B4"
+}
+```
+Or
+```json
+{
+    "cue_marker_color": "hotpink"
+}
+```
+Or with some opacity?
+
+```json
+{
+    "cue_marker_color": "rgba(245, 39, 145, 0.5)"
+}
+```
+![edit cue colour](readme_images/options.gif)
+
+### Installation
+Please follow the [well docemented guide on installing plugins](https://player.support.brightcove.com/coding-topics/step-step-plugin-development.html#Deploy_the_plugin) for a Brightcove player. This is considered to be a **custom** plugin as the references to the JavaScript file, CSS file and adding the JSON options are essential.\
+Key ingredients: The [javascript file](cuepoints.js), [the CSS file](cuepoints.css) and [adding options JSON](https://studio.support.brightcove.com/players/general/configuring-player-plugins.html#add_plugin) to the plugin configuration as _illustrated above_.
+#### It's all in the name: 
+Make sure the plugin is named `cuePointMarkersPlugin` in the `plugin name` field in the Video Cloud plyers module.
+Watch the video at the following Brightcove docs page on how to install a plugin. The guide details the plugin name quite well.
+https://player.support.brightcove.com/basics/developer-basics-custom-plugin-using-studio.html
+
+> :warning: **Disclaimer**
+> * As this plugin has been created for demonstration purposes it is not officially supported by Brightcove
+> * This plugin is not intended for production deployment. If this plugin is utilised for production deployment it is done so  at the users or entities risk. All appropriate testing, applicable coding standards and security is the responsibility of the deployer/user.
+> * It is the users/deployers responsibility to host the plugin files so they are accessible to the Brightcove player.
+> * This plugin is not covered under the Brightcove support agreement.
---
+++ plugin/cuepoints.js
@@ -0,0 +1,213 @@
+videojs.registerPlugin('cuePointMarkersPlugin', function(options) {
+	var player = this;
+    player.on('loadedmetadata', function() {
+        let cuePointsArr = new Array(),
+            filteredCueArr = new Array(),
+            purgedCueArr = new Array(),
+            // Define the video duration as a variable
+            videoDuration = player.mediainfo.duration,
+            // Get Video Cloud metadata long description field
+            longDesc = player.mediainfo.longDescription,
+            // Define video ID for playlist player management
+            videoId = player.mediainfo.id;
+        // Add existing cue point metadata to the cue points array
+        for (let i = 0; i < player.mediainfo.cuePoints.length; i++){
+            cuePointsArr.push(player.mediainfo.cuePoints[i]);
+        }
+        // Check for chapters in the longDescription metadata field, sort and merge
+        xtractMatch(longDesc, cuePointsArr, videoDuration, videoId);
+        // Add to filtered array based on original
+        filteredCueArr = cuePointsArr.filter(cue => (cue.type === 'CODE') || (cue.type === 'TEXT'));
+        // Remove duplicates based on time and replace
+        purgedCueArr = dedupeArr(filteredCueArr);
+        // Assign endTime to cue points in sorted array
+        assignCueEndTime(purgedCueArr,videoDuration);
+        // Take purged cue point information and add cue markers to player progress bar
+        addCueEl(purgedCueArr, videoDuration, options);
+        // If playlist player/playlist is present - clear UI for new playlist item
+        if (player.playlistUi !== undefined) player.on('playlistitem', function(){rmCueEl()})
+    })
+});
+
+const xtractMatch = (string, arr, videoDuration, videoId) => {
+    if (string === null) return(arrSort(arr));
+        // Match time formats M:SS, MM:SS, HH:MM:SS, H:MM:SS
+    let tRex = new RegExp(/(^(?:[01]\d|2[0-3]|[0-59]):[0-5]\d:[0-5]\d)|(^(?:[0-5]\d|2[123]|[0-59]):[0-5]\d)/gm),
+        // Match whole line that begins with 00: Lines with time format not at the beginning are ignored
+        dRex = new RegExp(/^.*?(^[0-5][0-9]:|^[0-59]:).*$/gm),
+        chaptrTime = string.match(tRex),
+        chaptrName = string.match(dRex);
+        // No found chapters - sort the array as it is and skip adding any further cue information
+    if (chaptrTime === null) return(arrSort(arr));
+    for (let i = 0; i < chaptrTime.length; i++) {
+        let time = chaptrTime[i].split(':'),
+            description = chaptrName[i].slice(chaptrTime[i].length),
+            seconds,
+            // Add ranomised 13 digit ID
+            idNum = Math.floor(Math.random() * 9000000000000) + 1000000000000;
+        // Strip hyphens and other intersting chars from string and trim whitespace    
+        description = stringTidy(description);
+        // push into array objects
+        timeConversion(arr, time, idNum, seconds, description, videoDuration, videoId);
+    }
+    // Sort array based on time
+    arrSort(arr);
+}
+
+// Array sort - order array based on time from lowest to highest
+const arrSort = (arr) => {
+    arr.sort((a, b) => {
+        return a.time - b.time;
+    });
+}
+
+// Removal of hyphens, pluses but allows inverted commas etc
+const stringTidy = (str) => {
+    str = str.replace(/([.,\/;:*{}=\-_~()<>{}+])/g, '');
+    // Remove whitespace form either end of the string
+    str = str.trim();
+    return(str);
+}
+
+const timeConversion = (arr, time, idNum, seconds, description, duration, videoId) => {
+    // Check for words in description if none set fields to blank
+    if (description.match(/\b[^\d\W]+\b/g) === null) description = '';
+    if (time.length === 2){
+        // Convert MM:SS to seconds
+        seconds = (Number.parseFloat(time[0]) * 60 + Number.parseFloat(time[1]));
+        // If chapter is longer than the video skip
+        if (seconds > duration) return;
+        arr.push({
+            id: `${idNum}`,
+            name: description,
+            type: 'TEXT',
+            time: seconds,
+            metadata: description,
+            startTime: seconds,
+            endTime: '',
+            video_id: videoId
+        });
+    }
+    if (time.length === 3){
+        // Convert HH:MM:SS to seconds
+        seconds = (Number.parseFloat(time[0]) * 3600 + Number.parseFloat(time[1]) * 60 + Number.parseFloat(time[2]));
+        // If chapter is longer than the video skip
+        if (seconds > duration) return;
+        arr.push({
+            id: `${idNum}`,
+            name: description,
+            type: 'TEXT',
+            time: seconds,
+            metadata: description,
+            startTime: seconds,
+            endTime: '',
+            video_id: videoId
+        });
+    }
+}
+
+// Filter array through map - remove duplicates
+const dedupeArr = (arr) => { 
+    let mapObj = new Map()
+    arr.forEach(v => {
+        let prevValue = mapObj.get(v.time)
+        if(!prevValue){
+            mapObj.set(v.time, v)   
+        }
+    })
+    return [...mapObj.values()];
+}
+
+// Hacky method to reassign endTime cue data in array after arrSort
+const assignCueEndTime = (arr, duration) => {
+    // Order the array on time again - for loop uses time from preceding array object 
+    arrSort(arr);
+    let v = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (v <= arr.length - 1)arr[i].endTime = arr[v].time;
+        // If last object in array set end time to video duration
+        if (v === arr.length) arr[i].endTime = duration;
+        v++;
+    }
+}
+
+// Build cue point markers and add them to the player progress bar
+const addCueEl = (arr, videoDuration, options) => {
+    let playerWidth = document.querySelector('.video-js').offsetWidth,
+        controlBar = document.querySelector('.vjs-progress-control'),
+        progresBar = document.querySelector('.vjs-progress-holder'),
+        cueControl = document.createElement('div'),
+        cueTip = document.createElement('div');
+    cueTip.className = 'vjs-cue-tip';
+    cueControl.className = 'vjs-cue-control';
+    cueControl.style.setProperty('--cue-control-width', playerWidth + 'px');
+    controlBar.prepend(cueControl);
+    progresBar.appendChild(cueTip);
+    // Loop through array and add elements
+    for (let i = 0; i < arr.length; i++) {
+        let el = document.createElement('div');
+        el.className = 'vjs-cue-marker';
+        el.id = 'marker-' + i;
+        el.style.setProperty('--marker-color', options.cue_marker_color);
+        // On mouse over event - add mouse event listener to cue marker elements
+        el.addEventListener("mouseover", (e) => {
+            setCueInfo(e, arr);
+        });
+        let time = arr[i].time;
+        // Based on proportion of width in px using time 
+        el.style.left = `${Math.round(time / videoDuration * playerWidth)}px`;
+        cueControl.append(el);
+    }
+    // Create the inner placeholder for the cue point data on the tooltips
+    createCueInfoEl();
+}
+
+// Remove created elements if playlist is present and new video has loaded
+const rmCueEl = () => {
+    let cueControl = document.querySelector('.vjs-cue-control'),
+        cueTip = document.querySelector('.vjs-cue-tip');
+    if (cueTip !== null) cueTip.remove();
+    if (cueControl !== null) cueControl.remove();
+}
+
+// Create and introduce to DOM the information tool data
+const createCueInfoEl = () => {
+    let cueTipData = document.createElement('p'),
+        cueTip = document.querySelector('.vjs-cue-tip');
+    cueTipData.className = 'vjs-cue-data';
+    cueTip.appendChild(cueTipData);
+}
+
+// Control mouse interaction with the cue markers - Initiated on hover state
+const setCueInfo = (e, arr) => {
+    let i = e.target.id.slice(7),
+        cueHolder = document.querySelector('.vjs-cue-control').offsetWidth,
+        cueMarker = document.querySelectorAll('.vjs-cue-marker')[i],
+        cueTip = document.querySelector('.vjs-cue-tip'),
+        cueTipData = document.querySelector('.vjs-cue-data');
+    cueTip.classList.add('vjs-cue-tip-visible');
+    cueTipData.innerHTML = `${arr[i].name}`;
+    cueTip.style.display = null;
+    // Display cue tool tip on left or right of marker on hover based on position
+    if (cueMarker.offsetLeft > cueHolder / 2){
+        cueTipData.classList.remove('vjs-cue-data-right');
+        cueTipData.classList.add('vjs-cue-data-left');
+        cueTip.style.left = null;
+        cueTip.style.right = cueHolder - cueMarker.offsetLeft +20 + 'px';
+    } else {
+        if (arr[i].time === 0) cueMarker.classList.add('initial-marker');
+        cueTipData.classList.remove('vjs-cue-data-left');
+        cueTipData.classList.add('vjs-cue-data-right');
+        cueTip.style.right = null;
+        cueTip.style.left = cueMarker.offsetLeft +20 + 'px';
+    }
+    if (arr[i].name === '') cueTip.style.display = 'none';
+    // Mouse move event - follow the mouse pointer on Y axis only
+    cueMarker.addEventListener('mousemove', (e) => {
+        cueTip.style.top = e.offsetY - 27 + 'px';
+    });
+    // On mouse out event - remove inline styles and classes
+    cueMarker.addEventListener('mouseout', () => {
+        cueTip.classList.remove('vjs-cue-tip-visible');
+    });
+}
